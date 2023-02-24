97c32da0c447298955b1648907fb790adb1ae718
tr_chun-horn <tr_chun-horn.heng@vitrox.com>
2023-02-24 15:55:57 +0800

    This is the first commit for my node js server demo

---
+++ server.js
@@ -0,0 +1,20 @@
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+//Must use this if need to parse parameter via http post/get etc.
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.post("/bmiCalculator.html", function(req,res){
+    var bmi = Number(req.body.weight) + Number(req.body.height);
+    res.send('The BMI calculated is ' + bmi);
+});
+
+app.listen(3000, ()=>{
+    console.log("Server started...")
+});
